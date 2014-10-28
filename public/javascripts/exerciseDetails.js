df1deb6ef7e0f5571c7294376c696783975c9e3b
GGunnison <GrantGun@hotmail.com>
2014-10-28 17:01:58 -0400

    no longer need

--- public/javascripts/exerciseDetails.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-	var data = {name: 'Bench', weight: '220', setCount: '3', repCount: '5'};
-  	$('#exerciseDetails').html(Handlebars.templates['exerciseDetails'](data));
-});
