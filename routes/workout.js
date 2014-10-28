bfbb8930a539d953f4cb330270e1ddad9138447e
Dirk <stahdirk@mit.edu>
2014-10-27 20:24:32 -0400

    both tests passing and functional, and also standalone

--- routes/workout.js
+++ routes/workout.js
@@ -1,34 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var utils = require('../utils/utils.js');
+var utils = require('../utils/utils');
 var Workouts = require('../models/Workout.js');
 var Exercises = require('../models/Exercise.js');
 //var ExerciseTemplate = require('../model/ExerciseTemplate.js');
 
-/* GET users listing. 
-router.get('/', function(req, res) {
-	console.log('IN /WORKOUT GET');
-	res.render('index');
-});*/
-
+//gets all workouts for a user
 router.get('/', function(req,res) {
-	var workoutID = req.body.workoutID;
+	console.log('in GET /');
+	var userID = req.query.userid;
+	console.log('userID: ');
+	console.log(userID);
+	Workouts.find({user: userID}, function(err, workouts) {
+		if (err) {
+			utils.sendErrResponse(res,500,'Could not retrieve workout from database');
+		}else{
+			utils.sendSuccessResponse(res, {workout: workouts});
+		}
+	});
+});
+
+//gets a single workout (designated by id) for a suer
+router.get('/single', function(req,res) {
+	console.log('in GET / single');
+	var workoutID = req.query.workoutID;
+	console.log('workoutID: ');
+	console.log(workoutID);
 	Workouts.findOne({_id: workoutID}, function(err, workout) {
 		if (err) {
 			utils.sendErrResponse(res,500,'Could not retrieve workout from database');
 		}else{
-			utils.sendSuccessResponse(res, workout);
+			utils.sendSuccessResponse(res, {workout: workout});
 		}
 	});
 });
 
 //POST add a workout
 router.post('/', function(req,res) {
-	console.log('in POST /workout');
+	console.log('in POST /');
 	//var currentUser = req.session.user; // <- uncomment once passport is working
 	var userid = req.body.userid; //using userID for now until we get Passport working.
-	console.log('userid: ');
-	console.log(userid)
+
 	//create workout for the current user, specified date, and with no exercises
 	var workout = new Workouts({user: userid, date: new Date('Jun 23, 1912'), exercises: []});
 	//save the workout to the DB
@@ -42,37 +54,61 @@ router.post('/', function(req,res) {
 		}
 	});
 });
-//
+
 router.put('/', function(req, res){
 	//var currentUser = req.session.user; //TODO: removed for testing
-	var currentUser = 'bob'; //added for testing
-	var newdate = req.body.date;
+	var currentUser = req.body.user; //added for testing
+	var workoutID = req.body.workoutID;
+	var newDate = req.body.date;
 
-	Workouts.update({user: currentUser/*removed ._id*/}, {$set: {date: newdate}}, function(err, workout){
-		if (err){
+	Workouts.findOne({_id: workoutID}, function(err,workout) {
+		workout.date = newDate;
+
+		workout.save(function(err) {
+			if (err) {
+				utils.sendErrResponse(res,500, "Could not save workout after edit");
+			}else{
+				//utils.sendSuccessResponse(res,workout);
+				Workouts.findOne({_id: workoutID}, function(err,workout) {
+					if (err) {
+						utils.sendErrResponse(res,500, "Could not retrieve after editing");
+					}else{
+						console.log('date after getting retrieving again in edit');
+						utils.sendSuccessResponse(res, {workout:workout});
+					}
+				});
+			}
+		});
+	});
+		
+	
+	//Workouts.update({user: currentUser/*._id*/}, {$set: {date: newdate}}, function(err){
+	/*	if (err){
 			utils.sendErrResponse(res, 500, "Could not update workout");
 		}else{
-			utils.sendSuccessResponse(res, {workout: workout, user: currentUser._id, date: workout.date, exercises: workout.exercises});
+			Workouts.findOne({user: currentUser}, function(err,workout) {
+				utils.sendSuccessResponse(res, workout);
+			});
 		}
 	});
+*/
 });
 
 //DELETE workout
 router.delete('/', function(req,res){
 	var workoutID = req.body.workoutID;
-	Workouts.remove({_id: workoutID}, function(err, workout){
+	Workouts.remove({_id: workoutID}, function(err){
 		if (err){
 			console.log("Could not delete workout");
 			utils.sendErrResponse(res, 500, "Could not delete from database")
 		}else{
-			console.log("Delete workout");
-			utils.sendSuccessResponse(res);
+			console.log("Deleted workout");
+			utils.sendSuccessResponse(res, {succeeded: true});
 		}
 	});
 });
 
 //POST add an Exercise to a Workout
-
 //eventually want to add info in from template instead of random form
 
 router.post('/exercises', function(req,res) {
@@ -87,17 +123,25 @@ router.post('/exercises', function(req,res) {
 	var exercise = new Exercises({name: exerciseName, description: description, repCount: repCount, 
 										setCount: setCount, weight: weight});
 
-	Workouts.update({_id: workoutID}, {$addToSet:{exercises: exercise}}, function(err, workout){
+	Workouts.findOne({_id: workoutID}, function(err, workout){
 		if (err){
 			utils.sendErrResponse(res, 500, 'Could not find workout with that ID');
 		}else{
-			workout.save(function(err){
-				if (err){
-					utils.sendErrResponse(res, 500, 'Could not save workout to database');
+			workout.exercises.push(exercise);
+			exercise.save(function(err) {
+				if (err) {
+					utils.sendErrResponse(res,500,'Could not save exercise');
 				}else{
-					utils.sendSuccessResponse(res, {workout: workout})
+					workout.save(function(err){
+						if (err){
+							utils.sendErrResponse(res, 500, 'Could not save workout to database');
+						}else{
+							utils.sendSuccessResponse(res, {workout: workout})
+						}
+					});
 				}
-			});
+			})
+
 		}
 	});
 });
@@ -132,9 +176,6 @@ router.post('/exercises', function(req,res) {
 // 	});
 // });
 
-//find exercise
-//delete exercise from db
-//done
 
 //get all data for new exercise
 //find exercise to change
@@ -143,14 +184,36 @@ router.post('/exercises', function(req,res) {
 
 
 router.put('/exercises', function(req, res){
-	var workoutID = req.body.workoutID; //the ID of the workout we want to add the exercise to
 	var exerciseName = req.body.name;
-	var description = req.body.description;
 	var repCount = req.body.repCount;
 	var setCount = req.body.setCount;
 	var weight = req.body.weight;
-	var time = req.body.time;
 	var exerciseID = req.body.exerciseID;
+
+	Exercises.findOne({_id: exerciseID}, function(err,exercise) {
+		if (!exercise) {
+			utils.sendErrResponse(res,500,'Could not find exercise in database');
+			return;
+		}
+		if (exerciseName) {
+			exercise.exerciseName = exerciseName;
+		}if(repCount) {
+			exercise.repCount = repCount;
+		}if(setCount) {
+			exercise.setCount = setCount;
+		}if(weight) {
+			exercise.weight = weight;
+		}
+		exercise.save(function(err) {
+			if(err) {
+				utils.sendErrResponse(res,500,'Could not update exercise');
+			}
+			else {
+				utils.sendSuccessResponse(res, {exercise: exercise});
+			}
+		});
+	});
+	/*
 	Exercises.update({_id: exerciseID}, {$set:{name: exerciseName, description: description, repCount: repCount,
 		setCount: setCount, weight: weight}}, function(err, exercise){
 			if (err){
@@ -160,6 +223,8 @@ router.put('/exercises', function(req, res){
 			}
 		});
 	});
+	*/
+});
 
 //delete exercise from workout/DB
 router.delete('/exercises', function(req,res) {
